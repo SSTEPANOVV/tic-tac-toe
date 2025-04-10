8d9a0da91c97fe36d025395cde797de2cd32efa9
Andrii Stepanov <andriystepanov2003@gmail.com>
2025-04-10 19:50:54 +0300

    Add winner display with player names instead of symbols

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,10 @@ function deriveActivePlayer(gameTurns) {
 }
 
 function App() {
+  const [players, setPlayers] = useState({
+    X: "Player1",
+    O: "Player2",
+  });
   const [gameTurns, setGameTurns] = useState([]);
 
   const activePlayer = deriveActivePlayer(gameTurns);
@@ -52,7 +56,7 @@ function App() {
       firstSquareSymbol === secondSquareSymbol &&
       firstSquareSymbol === thirdSquareSymbol
     ) {
-      winner = firstSquareSymbol;
+      winner = players[firstSquareSymbol];
     }
   }
 
@@ -78,6 +82,10 @@ function App() {
     setGameTurns([]);
   }
 
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers((prevPlayers) => ({ ...prevPlayers, [symbol]: newName }));
+  }
+
   return (
     <main>
       <div id="game-container">
@@ -86,11 +94,13 @@ function App() {
             initialName="Player 1"
             symbol="X"
             isActive={activePlayer === "X"}
+            onChangeName={handlePlayerNameChange}
           />
           <Player
             initialName="Player 2"
             symbol="O"
             isActive={activePlayer === "O"}
+            onChangeName={handlePlayerNameChange}
           />
         </ol>
         {(winner || hasDraw) && (
--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,13 +1,21 @@
 import { useState } from "react";
 
-import "./Player.css"
+import "./Player.css";
 
-export default function Player({ initialName, symbol, isActive }) {
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
   const [name, setName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
     setIsEditing((editing) => !editing);
+    if (isEditing) {
+      onChangeName(symbol, name);
+    }
   }
 
   function handleChangeName(event) {
